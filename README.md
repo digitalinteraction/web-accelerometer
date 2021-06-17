d916c1f096290b79dd6ae606f6951a044b3b3abf
Daniel Jackson <email@danieljackson.co.uk>
2021-06-17 16:16:19 +0100

    Rebuilt

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Web-based accelerometer capture example:
 
 <!--
 Minimal data capture example in [`docs/test.html`](https://digitalinteraction.github.io/web-accelerometer/test.html).
+
+To build (does not work on newer versions of node, but works on node 8.17.0): `npm run build` 
 -->
---
+++ docs/service-worker.js
@@ -0,0 +1,61 @@
+const urls = [
+  '/',
+  '/manifest.webmanifest',
+  '/icon64.png',
+  '/icon96.png',
+  '/icon48.png',
+  '/icon100.png',
+  '/icon128.png',
+];
+
+self.addEventListener('fetch', (event) => {
+  //console.log('SERVICEWORKER: Request: ' + event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(async (response) => {
+      if (response) {
+        console.log('SERVICEWORKER: Respond from cache: ' + event.request.url);
+        return response;
+      } else {
+        console.log('SERVICEWORKER: Respond with fetch: ' + event.request.url);
+        return await fetch(event.request);
+      }
+    })
+  );
+});
+
+self.addEventListener('install', function (event) {
+  console.log('SERVICEWORKER: Installing version: ' + cacheName);
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      //return cache.addAll(urls)
+      return Promise.all(
+        Array.from(urls.values()).map(function(url) {
+          const actualUrl = url + '?' + cacheName;    // Prevent cache
+          const request = new Request(actualUrl, {credentials: 'same-origin'});
+          return fetch(request).then(function(response) {
+            if (!response.ok) {
+                throw new Error('Request for ' + url + ' had status ' + response.status);
+            }
+            return cache.put(url, response);
+          });
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('SERVICEWORKER: Activating version: ' + cacheName);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key, i) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+const cacheName = 'v' /* STRING TO BE COMPLETED AT BUILD TIME */ + '20210617151537622';
